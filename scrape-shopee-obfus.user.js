33edec3a45b36fb12e30936295454e8e5299ad03
vinzmobile1 <salesvinzmobile1@gmail.com>
2025-07-07 15:56:22 +0700

    Create scrape-shopee-obfus.user.js

---
+++ scrape-shopee-obfus.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Shopee Scraper Obfus
+// @namespace    https://shopee.co.id/
+// @version      1.0.0
+// @description  Shopee scrape all in one
+// @author       Logic by Naufal Abiyyu
+// @match        https://shopee.co.id/*
+// @icon         https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/ca5d12cb33c0998393a11b61962377b4.png
+// @require      https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+// @updateURL    
+// @downloadURL  
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+
+
+})();
