5a23b4a9f5550ba462f3c6fc8c82616947cc100b
vinzmobile1 <salesvinzmobile1@gmail.com>
2025-07-07 13:43:08 +0700

    Update scrape-shopee.user.js

--- scrape-shopee.user.js
+++ scrape-shopee.user.js
@@ -14,8 +14,8 @@
 // @grant        unsafeWindow
 // @connect      script.google.com
 // @connect      script.googleusercontent.com
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/vinzmobile1/tampermonkey/raw/refs/heads/main/scrape-shopee.user.js
+// @downloadURL  https://github.com/vinzmobile1/tampermonkey/raw/refs/heads/main/scrape-shopee.user.js
 // ==/UserScript==
 
 (function() {
